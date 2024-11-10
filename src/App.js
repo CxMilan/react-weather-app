85abfbbc5609e3f3479256ba18074636e8aa3fdd
Cate Milan <catherine.milanetti@gmail.com>
2024-11-09 23:15:01 -0500

    added elements

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/98761-catherine-milanetti"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cate Milan
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/CxMilan/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/98761-catherine-milanetti"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cate Milan
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/CxMilan/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
