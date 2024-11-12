42b6f09bd8dad104a92c7747a5e07b19e5a40116
Cate Milan <catherine.milanetti@gmail.com>
2024-11-11 22:37:16 -0500

    added some javascript

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  const apiKey = "63214c4281922e3bb72fdf12dada7734";
-  
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-   return (
-     <div className="Weather">
-       <form>
-         <div className="row">
-           <div className="col-9">
-             <input
-               type="search"
-               placeholder="Enter a City..."
-               className="form-control"
-               autoFocus="on"
-             />
-           </div>
-           <div className="col-3">
-             <input
-               type="submit"
-               value="Search"
-               className="btn btn-primary w-100"
-             />
-           </div>
-         </div>
-       </form>
-       <h1>Laval</h1>
-       <ul>
-         <li>Wednesday 07:00</li>
-         <li>partly cloudy</li>
-       </ul>
-       <div className="row mt-3">
-         <div className="col-6">
-             <img
-               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-               alt="partly cloudy"
-             />
-               <span className="temperature">6</span>
-               <span className="unit">˚C</span>
-         </div>
-         <div className="col-6">
-           <ul>
-             <li>Precipitation: 15%</li>
-             <li>Humidity: 72%</li>
-             <li>Wind: 13km/h</li>
-           </ul>
-         </div>
-       </div>
-     </div>
-   );
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+    
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a City..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Wednesday 07:00</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="partly cloudy"
+            />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">˚C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind:{weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "15b6ba0523386a8a73b38b2440a74dea";
+    let city = "Laval";
+    let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
