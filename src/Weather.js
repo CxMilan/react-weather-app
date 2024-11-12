377cfae59ed60db0b089fd03571d94973bfd8b25
Cate Milan <catherine.milanetti@gmail.com>
2024-11-12 00:20:54 -0500

    added content

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "fe1483f743b581b5520a1b725af03a49";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
